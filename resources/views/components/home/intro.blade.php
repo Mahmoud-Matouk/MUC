fdad6d606f48b3a89e42eaa59cc3817003e92d18
Hossam Ahmed <hossamahmed201515@gmail.com>
2024-11-21 16:48:31 +0200

    header

--- resources/views/components/home/intro.blade.php
+++ resources/views/components/home/intro.blade.php
@@ -2,11 +2,12 @@
     <div class=" flex flex-row  bg-transparent items-center ">
         
         <div class="relative z-20 flex flex-col gap-2 w-[454px] mr-4">
-            <div class="flex gap-1">
-                <h2 class="text-[32px] leading-[48px] font-bold text-dark-950">{{__("front.home.intro.head")}}</h2>
+            <div class="flex flex-col gap-4">
+                <h2 class="text-[32px] leading-[48px] font-bold text-primary-800">{{__("front.home.intro.head")}}</h2>
+                <h2 class="text-[32px] leading-[48px] font-bold text-dark-800">{{__("front.home.intro.captions.caption1")}}</h2>
                 
             </div>
-            <p class="text-xl leading-[30px] font-normal text-dark-900">{{__("who.details")}}</p>
+            <p class="text-xl leading-[30px] font-normal text-dark-500">{{__("front.home.intro.captions.caption2")}}</p>
             <a href="#contact"
             class="py-3 px-[31px] z-10 overflow-hidden rounded-[25px] self-start  font-bold text-lg justify-center items-center text-white
               bg-secondary-800
@@ -17,7 +18,7 @@
         </div>
         <div class="absolute inset-0 left-0 z-10 ">
 
-            <img class="relative  w-[955px] h-[647.82px] " src="{{asset("assets/img/home/who/image.svg")}}" alt="image">
+            <img class="relative  w-[955px] h-[647.82px] mr-auto" src="{{asset("assets/img/home/intro/image.svg")}}" alt="image">
         </div>
 
     </div>
